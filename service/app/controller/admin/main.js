3559859147bf0b80ff659cdaf17e80041cd62451
ywhao2019 <245424341@qq.com>
2019-12-03 11:03:43 +0800

    jwt解析

--- service/app/controller/admin/main.js
+++ service/app/controller/admin/main.js
@@ -28,6 +28,8 @@ module.exports = app => {
   class MainController extends app.Controller {
     async index() {
       this.ctx.body = { code: 200, data: '登录状态验证成功' };
+      // const result = app.jwt.verify(this.ctx.session.token, app.config.jwt.secret);
+      // console.log(result.userName);
     }
 
     async checkLogin() {
@@ -39,7 +41,7 @@ module.exports = app => {
         // const openId = new Date().getTime();
         // this.ctx.session.openId = { openId };
         // this.ctx.body = { data: '登录成功', openId };
-        const token = app.jwt.sign({ userName, password }, app.config.jwt.secret);
+        const token = app.jwt.sign({ userName }, app.config.jwt.secret);
         this.ctx.session.token = token;
         this.ctx.body = { code: 200, data: '登录成功', token };
       } else {
