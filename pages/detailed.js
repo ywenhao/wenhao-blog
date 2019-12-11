3b8218f9214a7ec9c0efa4da524a70d3fb65fdf6
ywhao2019 <245424341@qq.com>
2019-12-11 16:11:42 +0800

    spin

--- pages/detailed.js
+++ pages/detailed.js
@@ -50,55 +50,55 @@ const Detailed = (props) => {
                 <title>Ywenhao's Blog - Detailed</title>
             </Head>
             <Header/>
-            <Row className="comm-main" type="flex" justify="center">
-                <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
-                    <div>
-                        <div className="bread-div">
-                            <Breadcrumb>
-                                <Breadcrumb.Item><a href="/">首页</a></Breadcrumb.Item>
-                                <Breadcrumb.Item>视频列表</Breadcrumb.Item>
-                                <Breadcrumb.Item>xxxx</Breadcrumb.Item>
-                            </Breadcrumb>
-                        </div>
+            <Spin spinning={isSpinning} tip="Loading...">
+                <Row className="comm-main" type="flex" justify="center">
+                    <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
                         <div>
-                            <div className="detailed-title">
-                                React实战视频教程-技术胖Blog开发(更新08集)
+                            <div className="bread-div">
+                                <Breadcrumb>
+                                    <Breadcrumb.Item><a href="/">首页</a></Breadcrumb.Item>
+                                    <Breadcrumb.Item>视频列表</Breadcrumb.Item>
+                                    <Breadcrumb.Item>xxxx</Breadcrumb.Item>
+                                </Breadcrumb>
                             </div>
-                            <div className="list-icon center">
-                                <span><Icon type="calendar"/>{props.addTime}</span>
-                                <span><Icon type="folder"/> {props.typeName}</span>
-                                <span><Icon type="fire"/> {props.view_count}人</span>
-                            </div>
-                            <div className="detailed-content">
-                                <Spin spinning={isSpinning} tip="Loading...">
-                                    <ReactMarkdown
-                                        source={markdown}
-                                        escapeHtml={false}
-                                    />
-                                </Spin>
+                            <div>
+                                <div className="detailed-title">
+                                    React实战视频教程-技术胖Blog开发(更新08集)
+                                </div>
+                                <div className="list-icon center">
+                                    <span><Icon type="calendar"/>{props.addTime}</span>
+                                    <span><Icon type="folder"/> {props.typeName}</span>
+                                    <span><Icon type="fire"/> {props.view_count}人</span>
+                                </div>
+                                <div className="detailed-content">
+                                        <ReactMarkdown
+                                            source={markdown}
+                                            escapeHtml={false}
+                                        />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </Col>
+                    </Col>
 
-                <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
-                    <Author/>
-                    <Advert/>
-                    <Affix offsetTop={5}>
-                        <div className="detailed-nav comm-box">
-                            <div className="nav-title">文章目录</div>
-                            <div className="toc-list">
-                                {tocify && tocify.render()}
+                    <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+                        <Author/>
+                        <Advert/>
+                        <Affix offsetTop={5}>
+                            <div className="detailed-nav comm-box">
+                                <div className="nav-title">文章目录</div>
+                                <div className="toc-list">
+                                    {tocify && tocify.render()}
+                                </div>
+                                {/* <MarkNav
+                                    className="article-menu"
+                                    source={markdown}
+                                    ordered={false}
+                                /> */}
                             </div>
-                            {/* <MarkNav
-                                className="article-menu"
-                                source={markdown}
-                                ordered={false}
-                            /> */}
-                        </div>
-                    </Affix>
-                </Col>
-            </Row>
+                        </Affix>
+                    </Col>
+                </Row>
+            </Spin>
             <Footer/>
         </>
     )
