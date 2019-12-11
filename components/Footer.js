cc1d262aae9fbb03951ace2a2edf2f01a0964ca5
ywhao2019 <245424341@qq.com>
2019-12-11 15:51:18 +0800

    backTop

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { BackTop } from 'antd'
 
 import '../static/style/components/footer.css'
 const Footer = ()=>(
     <div className="footer-div">
         <div>系统由 React+Node+Ant Desgin驱动 </div>
         <div>Ywenhao's Blog</div>
+        <BackTop />
     </div>
 )
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
-import { Row, Col, List, Icon, BackTop, Input } from 'antd'
+import { Row, Col, List, Icon, Input } from 'antd'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Author from '../components/Author'
@@ -98,7 +98,6 @@ const Home = (res) => {
         <Advert/>
       </Col>
     </Row>
-      <BackTop />
       <Footer />
   </>
 )};
