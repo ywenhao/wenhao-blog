d3677d2e7c5b4b77991971a167c2f41841d68ac0
ywhao2019 <245424341@qq.com>
2019-12-12 11:41:50 +0800

    v1.0

--- components/Author.js
+++ components/Author.js
@@ -1,4 +1,4 @@
-import { Avatar, Divider } from  'antd'
+import { Avatar, Divider, Tooltip } from  'antd'
 import '../static/style/components/author.css'
 
 const Author = () => {
@@ -8,7 +8,9 @@ const Author = () => {
             <div className="author-introduction">
                 前端程序员，专注于WEB和移动前端开发，此心随波逐流。
                 <Divider>社交账号</Divider>
-                <Avatar size={28} icon="github" className="account"  />
+                <Tooltip placement="top" title="https://github.com/ywenhao">
+                    <Avatar size={28} icon="github" className="account" />
+                </Tooltip>
                 <Avatar size={28} icon="qq"  className="account" />
                 <Avatar size={28} icon="wechat"  className="account"  />
             </div>
--- pages/index.js
+++ pages/index.js
@@ -51,46 +51,43 @@ const Home = (res) => {
     </Head>
     <Header type={type}/>
     <Row className="comm-main" type="flex" justify="center">
-      <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
-      <div>
-
-              <List
-                header={
-                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                        <div>
-                            最新日志
-                        </div>
-                        <div>
-                            <Search style={{width: 200}} placeholder="搜索文章" onSearch={onSearch} />
-                        </div>
+      <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={10}>
+          <List
+            header={
+                <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                    <div>
+                        最新日志
                     </div>
-                }
-                itemLayout="vertical"
-                dataSource={myList}
-                renderItem={item => (
-                  <List.Item>
-                    <div className="list-title">
-                      <Link href={{pathname:'/detailed',query:{id: item.id}}}>
-                        <a>{item.title}</a>
-                      </Link>
+                    <div>
+                        <Search style={{width: 200}} placeholder="搜索文章" onSearch={onSearch} />
                     </div>
-                    <div className="list-icon">
-                      <span><Icon type="calendar" />{item.addTime}</span>
-                      <span><Icon type="folder" /> {item.typeName}</span>
-                      <span><Icon type="fire" />{item.view_count}人</span>
-                    </div>
-                      <div className="list-context"
-                           dangerouslySetInnerHTML={{__html:marked(item.introduce)}}
-                      />
-                    <div className="list-more">
-                      <Link href={{pathname:'/detailed',query:{id: item.id}}}>
-                        <a>阅读全文 >>> </a>
-                      </Link>
-                    </div>
-                  </List.Item>
-                )}
-              />
-    </div>
+                </div>
+            }
+            itemLayout="vertical"
+            dataSource={myList}
+            renderItem={item => (
+              <List.Item>
+                <div className="list-title">
+                  <Link href={{pathname:'/detailed',query:{id: item.id}}}>
+                    <a>{item.title}</a>
+                  </Link>
+                </div>
+                <div className="list-icon">
+                  <span><Icon type="calendar" />{item.addTime}</span>
+                  <span><Icon type="folder" /> {item.typeName}</span>
+                  <span><Icon type="fire" />{item.view_count}人</span>
+                </div>
+                  <div className="list-context"
+                       dangerouslySetInnerHTML={{__html:marked(item.introduce)}}
+                  />
+                <div className="list-more">
+                  <Link href={{pathname:'/detailed',query:{id: item.id}}}>
+                    <a>阅读全文 >>> </a>
+                  </Link>
+                </div>
+              </List.Item>
+            )}
+          />
       </Col>
 
       <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
