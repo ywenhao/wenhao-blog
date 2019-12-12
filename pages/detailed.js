0a88cbec8fb9d7298c267a6aed9f15d56c93d20a
ywhao2019 <245424341@qq.com>
2019-12-12 11:48:57 +0800

    v1.0

--- pages/detailed.js
+++ pages/detailed.js
@@ -52,7 +52,7 @@ const Detailed = (props) => {
             <Header/>
             <Spin spinning={isSpinning} tip="Loading...">
                 <Row className="comm-main" type="flex" justify="center">
-                    <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
+                    <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={10}>
                         <div>
                             <div className="bread-div">
                                 <Breadcrumb>
