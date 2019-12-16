c54aa530f88ee29d382e8916bc08b78e0519eacb
ywhao2019 <245424341@qq.com>
2019-12-16 10:27:20 +0800

    sql 修改

--- service/app/controller/admin/main.js
+++ service/app/controller/admin/main.js
@@ -16,13 +16,16 @@ class MainController extends Controller {
     const article_content = this.ctx.request.body.articleContent;
     const addTime = this.ctx.request.body.showDate;
     const update_time = this.ctx.request.body.updateDate;
-    let sql;
+    // let sql;
+    let res;
     if (articleId) {
-      sql = `UPDATE article SET type_id = ${type_id}, title = '${title}', introduce = '${introduce}', article_content = '${article_content}', update_time = '${update_time}' WHERE id = ${articleId}`;
+      // sql = `UPDATE article SET type_id = ${type_id}, title = '${title}', introduce = '${introduce}', article_content = '${article_content}', update_time = '${update_time}' WHERE id = ${articleId}`;
+      res = await this.app.mysql.update('article', { id: articleId, type_id, title, introduce, article_content, update_time });
     } else {
-      sql = `INSERT INTO article(type_id, title, introduce, article_content, addTime) VALUES (${type_id}, '${title}', '${introduce}', '${article_content}', '${addTime}')`;
+      // sql = `INSERT INTO article(type_id, title, introduce, article_content, addTime) VALUES (${type_id}, '${title}', '${introduce}', '${article_content}', '${addTime}')`;
+      res = await this.app.mysql.insert('article', { type_id, title, introduce, article_content, addTime });
     }
-    const res = await this.app.mysql.query(sql);
+    // const res = await this.app.mysql.query(sql);
     if (res.affectedRows > 0) {
       this.ctx.body = { code: 200, data: '操作成功' };
     } else {
@@ -31,9 +34,10 @@ class MainController extends Controller {
   }
   async delArticle() {
     const id = this.ctx.request.body.id;
-    const sql = `DELETE FROM article WHERE id = ${id}`;
-    const results = await this.app.mysql.query(sql);
-    if (results.affectedRows > 0) {
+    // const sql = `DELETE FROM article WHERE id = ${id}`;
+    // const res = await this.app.mysql.query(sql);
+    const res = await this.app.mysql.delete('article', { id });
+    if (res.affectedRows > 0) {
       this.ctx.body = { code: 200, data: '操作成功' };
     } else {
       this.ctx.body = { code: 999, data: '操作失败' };
@@ -42,8 +46,9 @@ class MainController extends Controller {
   async checkLogin() {
     const userName = this.ctx.request.body.userName;
     const password = this.ctx.request.body.password;
-    const sql = `SELECT userName FROM admin_user WHERE userName = '${userName}' AND password = '${password}'`;
-    const res = await this.app.mysql.query(sql);
+    // const sql = `SELECT userName FROM admin_user WHERE userName = '${userName}' AND password = '${password}'`;
+    // const res = await this.app.mysql.query(sql);
+    const res = await this.app.mysql.select('admin_user', { userName, password });
     if (res.length > 0) {
       // const openId = new Date().getTime();
       // this.ctx.session.openId = { openId };
